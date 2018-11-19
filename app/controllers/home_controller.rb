ffe93e91358d348a9a5bd09fb7c45c3e5d033fbb
junggunlee <dream7boy>
2018-11-19 22:41:16 +0900

    test again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,5 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    byebug
-    shop = Shop.find_by_shopify_domain(test-junggun.myshopify.com)
-    shop.with_shopify_session do
-      ShopifyAPI::Product.all
-    end
-  end
-
-
-  def with_shopify_session(&block)
-    ShopifyAPI::Session.temp(shopify_domain, shopify_token, &block)
   end
 end
